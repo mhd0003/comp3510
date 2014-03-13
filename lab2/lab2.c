19fb482fd2886e5f4ce886d1550805e62e3f385b
Ian <Typhoan@users.noreply.github.com>
2014-03-12 21:49:42 -0500

    Update lab2.c
    This is untested, but I think this will do what he wants. Only thing to add is a way to tell how many devices there are. That way it will not post averages for all 32 devices.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,4 +1,3 @@
-
 /*****************************************************************************\
 * Laboratory Exercises COMP 3510                                              *
 * Author: Saad Biaz                                                           *
@@ -29,16 +28,23 @@
 /*****************************************************************************\
 *                            Global data structures                           *
 \*****************************************************************************/
-
-
-
+//to keep track of events, response, and turnaround for each device. 
+typedef struct DeviceTag{
+Timestamp responseTotal;
+Timestamp turnaroundTotal;
+int eventsProcessed = 0;
+int responses;
+int turnarounds;
+} Device;
 
 /*****************************************************************************\
 *                                  Global data                                *
 \*****************************************************************************/
-
-
-
+Device devices[32];
+Event events[32000];
+int front;
+int back;
+int deviceNum;
 
 /*****************************************************************************\
 *                               Function prototypes                           *
@@ -76,10 +82,35 @@ int main (int argc, char **argv) {
  \***********************************************************************/
 void Control(void){
 
-  while (1);
-
+	while (1)
+	{
+		if (Flags)
+		{
+			Status tempFlags = Flags;
+			Flags = 0;
+			
+			while(tempFlags)
+			{
+				deviceNum = 0;
+				if (tempFlags & 1)
+				{
+					InterruptRoutineHandlerDevice();
+				}
+				tempFlags = tempFlags >> 1;
+				deviceNum++;
+			}
+			while (front < back)
+			{
+				Event e = events[back];
+				Server(e);
+				devices[e.DeviceID].turnaroundTotal += Now() - e.When;
+				devices[e.DeviceID].turnarounds++;
+				devices[e.DeviceID].eventsProcessed++;
+				front++;
+			}
+		}
+	}
 }
-
 
 /***********************************************************************\
 * Input : None                                                          *
@@ -88,8 +119,12 @@ void Control(void){
 *           The id of the device is encoded in the variable flag        *
 \***********************************************************************/
 void InterruptRoutineHandlerDevice(void){
-  printf("An event occured at %f  Flags = %d \n", Now(), Flags);
-	// Put Here the most urgent steps that cannot wait
+  	printf("An event occured at %f  Flags = %d \n", Now(), TempFlags);
+	events[back] = &BufferLastEvent[deviceNum];
+	devices[deviceNum].responseTotal += Now() - events[back].When;
+	devices[deviceNum].responses++;
+	DisplayEvent('c', &BufferLastEvent[deviceNum]);
+	back++;
 }
 
 
@@ -104,9 +139,27 @@ void BookKeeping(void){
   // 1) the percentage of missed events, 2) the average response time, and 
   // 3) the average turnaround time.
   // Print the overall averages of the three metrics 1-3 above
+  int n = 0;
+  Timestamp avgResponse = 0;
+  Timestamp avgTurnaround = 0;
+  float percentMissed;
+  float avgPercentMissed = 0.0;
+  while(n </*number of devices*/)
+  {
+  		avgResponse += devices[n].responseTotal;
+		avgTurnaround += devices[n].turnaroundTotal;
+		devices[n].responseTotal = devices[n].responseTotal / devices[n].responses;
+		devices[n].turnaroundTotal = devices[n].turnaroundTotal / devices[n].turnarounds++;
+		percentMissed = (100 - devices[n].eventsProcessed) / 100;
+		avgPercentMissed += percentMissed;
+		
+		printf("\n Device %d: Avg Response: %4.3f Avg Turnaround: %4.3f Percent Missed: %4.3f\n",
+      n, devices[n].responseTotal, devices[n].turnaroundTotal, percentMissed);
+		n++;
+  }
+  printf("\n Averages over all devices: Avg Response: %4.3f Avg Turnaround: %4.3f Avg Percent Missed: %4.3f\n",
+  avgResponse/n, avgTurnaround/n, avgPercentMissed/n);
 }
-
-
 
 
 
